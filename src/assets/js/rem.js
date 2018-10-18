fc6c3415e7f75c7f7756ad1feb9f612f39d3db9b
biangz <nnuauroral@163.com>
2018-10-18 18:30:17 +0800

    可视区域的宽度 -> 元素宽度

--- src/assets/js/rem.js
+++ src/assets/js/rem.js
@@ -2,9 +2,11 @@
   var docEl = doc.documentElement
   var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
   var recalc = function () {
+    var offsetWidth = docEl.offsetWidth
     var clientWidth = docEl.clientWidth
-    if (!clientWidth) return
-    docEl.style.fontSize = 100 * (clientWidth / 375) + 'px'
+    console.log(offsetWidth, clientWidth)
+    if (!offsetWidth) return
+    docEl.style.fontSize = 100 * (offsetWidth / 375) + 'px'
   }
   if (!doc.addEventListener) return
   win.addEventListener(resizeEvt, recalc, false)
